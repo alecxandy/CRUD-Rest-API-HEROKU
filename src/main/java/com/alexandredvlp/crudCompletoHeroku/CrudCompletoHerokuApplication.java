9c301085dd67c3734ce0a00292996d40f6744b71
alecxandy <alecxandy@gmail.com>
2021-07-11 01:14:40 -0300

    version 11.07.21

---
+++ src/main/java/com/alexandredvlp/crudCompletoHeroku/CrudCompletoHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.alexandredvlp.crudCompletoHeroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCompletoHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCompletoHerokuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexandredvlp/crudCompletoHeroku/CrudCompletoHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandredvlp.crudCompletoHeroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCompletoHerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
